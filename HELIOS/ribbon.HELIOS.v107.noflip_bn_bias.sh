f9afe0051af92932df5213920ffb19106bea35ff
Ricardo Pizarro <ricardo.pizarro@mcgill.ca>
2019-01-31 13:36:28 -0500

    increase walltime for arch v107 noflip_bn_bias

--- HELIOS/ribbon.HELIOS.v107.noflip_bn_bias.sh
+++ HELIOS/ribbon.HELIOS.v107.noflip_bn_bias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N ribbon
 #PBS -A ngt-630-aa
-#PBS -l walltime=3:30:00
+#PBS -l walltime=4:00:00
 #PBS -l nodes=1:gpus=1
 #PBS -l feature=k80
 #PBS -o $HOME/histo/weights/xplor/noflip_bn_bias/v107/out/$(USER)_$(JOBID)_$(JOBNAME).out
