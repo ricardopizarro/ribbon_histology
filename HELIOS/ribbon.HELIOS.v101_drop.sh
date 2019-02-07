4eeb9c64f984a725d7e822e548811bfe136ccc01
Ricardo Pizarro <ricardo.pizarro@mcgill.ca>
2019-02-07 13:20:02 -0500

    submit jobs for v101_drop and v101_drop2

---
+++ HELIOS/ribbon.HELIOS.v101_drop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -N ribbon
+#PBS -A ngt-630-aa
+#PBS -l walltime=3:00:00
+#PBS -l nodes=1:gpus=1
+#PBS -l feature=k80
+#PBS -o $HOME/histo/weights/NN_arch/v101_drop/out/$(USER)_$(JOBID)_$(JOBNAME).out
+#PBS -e $HOME/histo/weights/NN_arch/v101_drop/out/rpizarro_${MOAB_JOBID}_ribbon.err
+
+source /home/rpizarro/.deep_env
+
+cd /home/rpizarro/histo/src
+python ribbon.train_unet.py 101_drop 100
+
+cd /home/rpizarro/histo/src/HELIOS
+# msub ribbon.HELIOS.v101_drop.sh
+
