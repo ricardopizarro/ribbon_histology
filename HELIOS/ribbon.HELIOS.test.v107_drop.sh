66b96b14d4dc954af29999d14c7525de48534b81
Ricardo Pizarro <ricardo.pizarro@mcgill.ca>
2019-02-27 21:50:15 -0500

    testing for the v_drop architecture

---
+++ HELIOS/ribbon.HELIOS.test.v107_drop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N ribbon
+#PBS -A ngt-630-aa
+#PBS -l walltime=6:00:00
+#PBS -l nodes=1:gpus=1
+#PBS -l feature=k80
+#PBS -o $HOME/histo/prediction/NN_arch/v107_drop/out/$(USER)_$(JOBID)_$(JOBNAME).out
+#PBS -e $HOME/histo/prediction/NN_arch/v107_drop/out/rpizarro_${MOAB_JOBID}_ribbon.err
+
+source /home/rpizarro/.deep_env
+
+cd /home/rpizarro/histo/src
+python ribbon.test_unet.py 107_drop 100
+
+
